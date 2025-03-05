9f062ea8b8b90d4a64febc370cf9f3f9499e7da5
NestorSandoval <nestordanielsg2001@gmail.com>
2025-03-04 21:45:04 -0600

    Add initial project files

---
+++ src/stores/recipeSlice.ts
@@ -0,0 +1,25 @@
+import { StateCreator } from "zustand";
+import { getCategories } from "../services/RecipeService";
+import type { Categories, SearchFilter } from "../types";
+
+export type RecipesSliceType = {
+  categories: Categories;
+  fetchCategories: () => Promise<void>;
+  searchRecipes: (searchFilters: SearchFilter) => Promise<void>;
+};
+
+export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
+  categories: {
+    drinks: [],
+  },
+  fetchCategories: async () => {
+    const categories = await getCategories();
+    set({
+      categories,
+    });
+  },
+
+  searchRecipes: async (filters) => {
+    console.log(filters);
+  },
+});
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+import {
+  CategoriesAPIResponseSchema,
+  SearchFilterSchema,
+} from "../utils/recipes-schema";
+
+export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
+export type SearchFilter = z.infer<typeof SearchFilterSchema>;
